be744a6c1ff34c5d06ba25a8aff4b4f8267b6962
AnthonyBeato <axba0001@ce.pucmm.edu.do>
2024-08-03 12:40:17 -0400

    Implementaion Gazebo

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,44 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+
+    # Incluyendo el robot_state_publisher launch file, provisto por nuestro paquete
+    package_name='robot_hardware' 
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Incluyendo el launch file de gazebo, provisto por el paquete gazebo_ros 
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    # Correr el nodo spawner del paquete de gazebo_ros
+    # ... poniendo nombre de la entidad con nombres dependiendo de la cantidad de robots activos
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'ti-rslk-i'],
+                        output='screen')
+
+
+    # Launch a los files
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
